8d21a8dc25c5ec785992e6daca121f5f5dead8e8
HRim-dev <gpfla46771@daum.net>
2021-01-03 21:53:03 +0900

    네이버 로그인 기능 추가

--- src/main/java/com/HrimDev/project/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/HrimDev/project/springboot/config/auth/dto/OAuthAttributes.java
@@ -31,6 +31,11 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes){
+
+        //네이버 아이디로 로그인
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName,attributes);
     }
 
@@ -43,6 +48,19 @@ public class OAuthAttributes {
                               .attributes(attributes)
                               .nameAttributeKey(userNameAttributeName)
                               .build();
+    }
+
+    //naver 로그인
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     //User 엔터티 생성->처음 가입했을 때
